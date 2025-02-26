be6631bc46ba996c875989e40d193ef8bd4fb078
Marko Lukac <marko.lukac@clustertec.com>
2025-02-26 11:21:24 +0100

    replace favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,4 +17,27 @@ export default defineNuxtConfig({
       calApiUrl: process.env.CAL_API_URL || "https://api.cal.com/v2",
     },
   },
+  app: {
+    head: {
+      title: "Lettebadi",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Lettebadi - Eine der beliebtesten Badeanstalten der Stadt",
+        },
+        { property: "og:title", content: "Lettebadi" },
+        {
+          property: "og:description",
+          content: "Lettebadi - Eine der beliebtesten Badeanstalten der Stadt",
+        },
+        { property: "og:image", content: "/media/event-type-1.jpg" },
+        { property: "og:url", content: "https://www.lettebadi.ch" },
+        { property: "og:type", content: "website" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
 });
